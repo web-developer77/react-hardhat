c569ee943aee731e0d8e7dda12821e8d4301bc75
web-developer77 <koa06800@gmail.com>
2022-05-28 13:34:48 -0700

    added module to read classtoken info

---
+++ frontend/src/components/ReadERC20.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { Text } from "@chakra-ui/react";
+import { ERC20ABI as abi } from "abi/ERC20ABI";
+import { ethers } from "ethers";
+interface Props {
+  addressContract: string;
+  currentAccount: string | undefined;
+}
+
+declare let window: any;
+
+export default function ReadERC20(props: Props) {
+  const addressContract = props.addressContract;
+  const currentAccount = props.currentAccount;
+  const [totalSupply, setTotalSupply] = useState<string>();
+  const [symbol, setSymbol] = useState<string>("");
+  const [balance, SetBalance] = useState<number | undefined>(undefined);
+
+  //call when currentAccount change
+  useEffect(() => {
+    if (!window.ethereum) return;
+    if (!currentAccount) return;
+
+    queryTokenBalance(window);
+  }, [currentAccount]);
+
+  async function queryTokenBalance(window: any) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const erc20 = new ethers.Contract(addressContract, abi, provider);
+
+    erc20
+      .balanceOf(currentAccount)
+      .then((result: string) => {
+        SetBalance(Number(ethers.utils.formatEther(result)));
+      })
+      .catch("error", console.error);
+  }
+
+  useEffect(() => {
+    if (!window.ethereum) return;
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const erc20 = new ethers.Contract(addressContract, abi, provider);
+    erc20
+      .symbol()
+      .then((result: string) => {
+        setSymbol(result);
+      })
+      .catch("error", console.error);
+
+    erc20
+      .totalSupply()
+      .then((result: string) => {
+        setTotalSupply(ethers.utils.formatEther(result));
+      })
+      .catch("error", console.error);
+    //called only once
+  }, []);
+
+  return (
+    <div>
+      <Text>ERC20 Contract: {addressContract}</Text>
+      <Text>
+        <b>ClassToken totalSupply</b>:{totalSupply} {symbol}
+      </Text>
+      <Text my={4}>
+        <b>ClassToken in current account</b>: {balance} {symbol}
+      </Text>
+    </div>
+  );
+}
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { Layout } from 'components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ChakraProvider>
+        <Layout>
+        <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+  )
 }
 
 export default MyApp
