4da2da1c4968224dc4fe08ac7858da3819c47625
web-developer77 <koa06800@gmail.com>
2022-05-28 10:12:49 -0700

    updated hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,32 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  defaultNetwork: "rinkeby",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1500,
+      },
+    },
+  },
+
+  // uncomment this and run: yarn deploy-rinkeby
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/0c430968c9bd401fa95f3d48ad84a45f`,
+      accounts: [0x827fd6b44e118fc8bbea6a65a201d3c69f49619c2eb74f946e14f0bf1d68f98c],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
